8e3a0d9adfd5ed748bb7c6da2a091f5ccc189b33
keymarrow <pkimaro15@gmail.com>
2021-10-18 12:22:10 +0300

    web stracture updated

--- loginhtml.php
+++ loginhtml.php
@@ -4,28 +4,30 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-	<meta charset="utf-8">
+	<link rel="stylesheet" href="login.css">
 	<title>login page</title>
 </head>
 <body>
-	<center>
+	<div class="login-tab">
+		<h1>Log In</h1>
 		<form action = "login.php" method = "post" id = "loginform">
 
-		<input type="text" name="idnumber" placeholder="enter ID number">
+		<input type="text" name="idnumber" placeholder="enter ID number" id="id-num">
 		<br><br>
-		<input type="password" name="pwd" placeholder="enter password">
+		<input type="password" name="pwd" placeholder="enter password" id="pass">
 		<br><br>
-		<button name = "login">login</button>
+		<div id="loginbtn"><button name = "login">login</button></div>
 		<br>
-		<?php 
+		<div id="forgetlink"><a href="#" id="forgetlink">I forgot my password</a></div> <br><br>
+		<!-- <?php 
 			if (isset($_SESSION['login'])) {
 			 	echo "<label for = 'loginform' style = 'color:red'>*invalid ID or password*</label>";	
 			} 
-		 ?>
-		<p>or <a href="registration.php">create account</a></p>
+		 ?> -->
+		<!-- <p>or <a href="registration.php">create account</a></p> -->
+		<div id="or-content"><p class="orcenter">or</p></div>
+		<div id="goggle"><button name = "login" id="goggle-btn"><a href="www.goggle.account.com">Login with Goggle</a></button></div>
 	    </form>
-	</center>
+	</di>
 </body>
 </html>
