c104e4323f7bc8b8330463e102ddb36251421f47
keymarrow <pkimaro15@gmail.com>
2021-10-22 15:01:06 +0300

    web structure updated

--- loginhtml.php
+++ loginhtml.php
@@ -11,12 +11,17 @@
 	<div class="login-tab">
 		<h1>Log In</h1>
 		<form action = "login.php" method = "post" id = "loginform">
-
-		<input type="text" name="idnumber" placeholder="enter ID number" id="id-num">
-		<br><br>
+		<div>
+			<input type="text" name="idnumber" placeholder="enter ID number" id="id-num">
+			<br><br>
+		</div>
 		<input type="password" name="pwd" placeholder="enter password" id="pass">
 		<br><br>
-		<div id="loginbtn"><button name = "login" href="main.html">login</button></div>
+		<div id="loginbtn"><button name = "login">login</button>
+		<div id="signUP">
+			<a href="registration.php">Or Sign Up</a>
+		</div>
+		</div>
 		<br>
 		<div id="forgetlink"><a href="#" id="forgetlink">I forgot my password</a></div> <br><br>
 		<!-- <?php 
@@ -28,10 +33,6 @@
 		<!-- <div id="or-content"><p class="orcenter">or</p></div>
 		<div id="goggle"><button name = "login" id="google-btn"><a href="https://accounts.google.com/o/oauth2/auth/oauthchooseaccount?client_id=689286984790-d8pi01ip72qttb4mugpli1bs2htc76g7.apps.googleusercontent.com&redirect_uri=https%3A%2F%2Fdashboard.ngrok.com%2Flogin%2Fgoogle%2Fauthorize&response_type=code&scope=email&state=yPcylBKFGBAU0ej9SORDaJEtC8GFe9VEq70ooWRGeXI&flowName=GeneralOAuthFlow">Login with Goggle</a></button></div> -->
 	    </form><br>
-		<div id="signUP">
-			<p>Don't have Account?</p>
-			<a href="registration.php">Sign Up</a>
-		</div>
 	</div>
 </body>
 </html>
