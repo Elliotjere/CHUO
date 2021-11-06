cf226e8d3d47b25dc3abbb5e9f89099be7d58e7d
Elliot jere <elliotjere28@gmail.com>
2021-11-06 17:25:47 +0300

    fixed some issues(bugs causing failure to login)

--- main.php
+++ main.php
@@ -10,17 +10,20 @@
 	<title></title>
 </head>
 <body id="body">
-<p style="color:green" id="welcome">welcome <b><?php if(isset($_SESSION['login'])){echo $_SESSION['name'];} ?></b></p>
 
-<form method="post">
+<a href="#"><img src="menu1.png" style="width: 30px; length: 30px"></a>
+
+<p style="color:green; font-family: arial" id="welcome">welcome <b><?php if(isset($_SESSION['login'])){echo $_SESSION['name'];} ?></b></p>
+
+<form method="post"> 
 	<button type = "logout" name = "logout">logout</button>
-	<?php
+	<?php 
 		if (isset($_POST['logout'])) {
 		 	session_destroy();
 		 	header("location:loginhtml.php");
 		 } 
 	 ?>
 </form>
-
+<img src="defaultpic.png" style="width : 50px; length: 50px; position:absolute; top:5px; right:5px;" align="right">
 </body>
 </html>
