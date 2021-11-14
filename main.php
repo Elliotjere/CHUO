74204b6122c3a4ff71e8a48047c5236211c59553
keymarrow <pkimaro15@gmail.com>
2021-11-14 09:36:08 +0300

    web background changed

--- main.php
+++ main.php
@@ -7,6 +7,8 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 	<meta charset="utf-8">
+	<link rel="stylesheet" href="main.css">
+
 	<title></title>
 </head>
 <body id="body">
